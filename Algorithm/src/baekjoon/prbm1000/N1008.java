99c4e69dc1b4ca0f46a16c4772581f547e8c6a52
wynn-oh <wynnoh31@gmail.com>
2024-04-26 20:06:30 +0900

    2024.04.26 v0.03
    Baekjoon is uploaded.

---
+++ Algorithm/src/baekjoon/prbm1000/N1008.java
@@ -0,0 +1,18 @@
+package baekjoon.prbm1000;
+
+import java.util.Scanner;
+
+public class N1008 {
+
+    public static void main(String[] args) {
+       Scanner scnr = new Scanner(System.in);
+       
+       double prmtr1 = scnr.nextDouble();
+       double prmtr2 = scnr.nextDouble();
+       
+       System.out.println(prmtr1 / prmtr2);
+       
+       scnr.close();
+    }
+
+}
---
+++ Algorithm/src/baekjoon/prbm1000/N10998.java
@@ -0,0 +1,18 @@
+package baekjoon.prbm1000;
+
+import java.util.Scanner;
+
+public class N10998 {
+
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        
+        int prmtr1 = scnr.nextInt();
+        int prmtr2 = scnr.nextInt();
+        
+        System.out.println(prmtr1 * prmtr2);
+        
+        scnr.close();
+    }
+
+}
